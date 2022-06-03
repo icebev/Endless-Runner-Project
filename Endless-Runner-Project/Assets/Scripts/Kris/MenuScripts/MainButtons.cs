adc42b2de202d4b524ef64e860b020089d9d1773
TwistyC <42874167+TwistedC@users.noreply.github.com>
2022-06-03 17:00:06 +0100

    Push 2
    No progress

--- Endless-Runner-Project/Assets/Scripts/Kris/MenuScripts/MainButtons.cs
+++ Endless-Runner-Project/Assets/Scripts/Kris/MenuScripts/MainButtons.cs
@@ -1,9 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class ButtonHandler : MonoBehaviour
+
+public class MainButtons : MonoBehaviour
 {
+
+    [SerializeField] private GameObject[] Buttons;
+
+
+    public void DisableButtons()
+    {
+        for (int i = 0; i <= this.Buttons.Length; i++)
+        {
+            Button TempButton = this.Buttons[i].GetComponent<Button>();
+            
+
+        }
+    }
+    public void EnableButtons()
+    {
+
+    }
     // Start is called before the first frame update
     void Start()
     {
--- Endless-Runner-Project/Assets/Scripts/Kris/MenuScripts/MenuHandler.cs
+++ Endless-Runner-Project/Assets/Scripts/Kris/MenuScripts/MenuHandler.cs
@@ -1,10 +1,82 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class MainMenuHandler : MonoBehaviour
+public class MenuHandler : MonoBehaviour
 {
     // Start is called before the first frame update
+    private MainButtons _MainButtons;
+    [SerializeField] private GameObject _MainHandler;
+
+
+    [SerializeField] private GameObject _OptionsHandler;
+
+
+    [SerializeField] private GameObject _CreditsHandler;
+
+    private enum gameMenus
+    {
+        Main,
+        Options,
+        Credits
+
+    }
+
+    private int CurrentState = (int)gameMenus.Main;
+
+    public int GetMenuState()
+    {
+        return this.CurrentState;
+        
+
+    }
+
+    public void SetMenuState(int MenuState)
+    {
+        if (this.CurrentState != MenuState && MenuState <= System.Enum.GetValues(typeof(gameMenus)).Length && MenuState >= 0)
+        {
+            switch (MenuState)
+            {
+                case (int)gameMenus.Main:
+                    _MainHandler.SetActive(true);
+
+                    break;
+                case (int)gameMenus.Options:
+                    _OptionsHandler.SetActive(true);
+
+                    break;
+                case (int)gameMenus.Credits:
+                    _CreditsHandler.SetActive(true);
+
+                    break;
+            }
+
+
+            switch (this.CurrentState)
+            {
+                case (int)gameMenus.Main:
+                    _MainHandler.SetActive(false);
+
+                    break;
+                case (int)gameMenus.Options:
+                    _OptionsHandler.SetActive(false);
+
+                    break;
+                case (int)gameMenus.Credits:
+                    _CreditsHandler.SetActive(false);
+
+                    break;
+
+            }
+            this.CurrentState = MenuState;
+
+        }
+
+       
+
+    }
+
     void Start()
     {
         
