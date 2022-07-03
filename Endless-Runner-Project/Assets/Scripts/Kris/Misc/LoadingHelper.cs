5ea1732c85c97ae7d1160893d097ddf1c8f0138c
TwistyC <42874167+TwistedC@users.noreply.github.com>
2022-07-03 20:15:24 +0100

    loading experimentation

--- Endless-Runner-Project/Assets/Scripts/Kris/Misc/LoadingHelper.cs
+++ Endless-Runner-Project/Assets/Scripts/Kris/Misc/LoadingHelper.cs
@@ -1,20 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class LoadingHelper : MonoBehaviour
 {
+
+    [SerializeField] private TextMeshProUGUI loadingText;
+    private string[] loadingTextIndex = new string[] {"Loading","Loading.", "Loading..", "Loading..."};
+    private int loadingTextTime = 0;
+
     // Start is called before the first frame update
     void Start()
     {
         GameObject _loadingManagerObject = GameObject.FindGameObjectWithTag("LoadManager");
         LoadingManager _loadingManagerScript = _loadingManagerObject.GetComponent<LoadingManager>();
         _loadingManagerScript.LoadGameScene3();
+        StartCoroutine(this.LoadingTextUpdate());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private IEnumerator LoadingTextUpdate()
+    {
+        yield return new WaitForSeconds(0.5f);
+        this.loadingTextTime += 1;
+        if(this.loadingTextTime > 3) { this.loadingTextTime = 0; }
+        this.loadingText.text = this.loadingTextIndex[this.loadingTextTime];
+        StartCoroutine(this.LoadingTextUpdate());
+        yield return null;
     }
 }
