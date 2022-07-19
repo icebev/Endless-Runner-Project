169e29035e71508180900ab3ba9bec6b0bf9bd75
Joe Bevis <49538329+icebev@users.noreply.github.com>
2022-07-19 12:55:18 +0100

    powerup prototypes test

--- Endless-Runner-Project/Assets/Scripts/Joe/Collectables System/CollectableEventFunctions.cs
+++ Endless-Runner-Project/Assets/Scripts/Joe/Collectables System/CollectableEventFunctions.cs
@@ -15,12 +15,17 @@ public class CollectableEventFunctions : MonoBehaviour
 
     public TextMeshProUGUI coinCountText;
 
+    public GameObject coinMagnetField;
+    public GameObject coinMagnetIndicator;
+
+    public GameObject coinMultiplierIndicator;
+
     private void Awake()
     {
         CollectableEventFunctions.OnCoinCollect = new UnityEvent<int>();
         CollectableEventFunctions.OnPowerUpCollect = new UnityEvent<PowerUpType>();
         CollectableEventFunctions.OnCoinCollect.AddListener(IncrementCoinCount);
-        CollectableEventFunctions.OnPowerUpCollect.AddListener(LogPowerUpType);
+        CollectableEventFunctions.OnPowerUpCollect.AddListener(TriggerPowerUp);
 
     }
 
@@ -30,8 +35,40 @@ public class CollectableEventFunctions : MonoBehaviour
         this.coinCountText.text = this.coinsCollected.ToString();
     }
 
-    public void LogPowerUpType(PowerUpType powerUpRef)
+    public void TriggerPowerUp(PowerUpType powerUpRef)
     {
-        //Debug.Log(powerUpRef.powerUpName);
+        if (powerUpRef.powerUpName == "CoinMagnet")
+        {
+            this.coinMagnetField.SetActive(true);
+            this.coinMagnetIndicator.SetActive(true);
+
+            StartCoroutine(DeactivateCoinMagnet(powerUpRef.powerUpDuration));
+
+        }
+
+        if (powerUpRef.powerUpName == "CoinMultiplier")
+        {
+            CoinCollectable.coinValueMultiplier = 2;
+            this.coinMultiplierIndicator.SetActive(true);
+
+            StartCoroutine(DeactivateCoinMultiplier(powerUpRef.powerUpDuration));
+
+        }
+    }
+
+    public IEnumerator DeactivateCoinMagnet(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        this.coinMagnetField.SetActive(false);
+        this.coinMagnetIndicator.SetActive(false);
+    }
+
+    public IEnumerator DeactivateCoinMultiplier(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        CoinCollectable.coinValueMultiplier = 1;
+        this.coinMultiplierIndicator.SetActive(false);
     }
 }
