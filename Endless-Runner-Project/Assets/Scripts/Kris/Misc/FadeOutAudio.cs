40eb8db2431263a69dbfb22757d9da7d17dade35
TwistyC <42874167+TwistedC@users.noreply.github.com>
2022-08-17 21:33:39 +0100

    Comment Part 3
    e

--- Endless-Runner-Project/Assets/Scripts/Kris/Misc/FadeOutAudio.cs
+++ Endless-Runner-Project/Assets/Scripts/Kris/Misc/FadeOutAudio.cs
@@ -3,20 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 
+/* FADE OUT AUDIO CLASS
+ * Author(s): Kris Burgess-James
+ * Date last modified: 17/08/2022
+ *******************************************************************************
+ *
+ */
+/// <summary>
+/// A class for fading out the music audio;
+/// </summary>
 public class FadeOutAudio : MonoBehaviour
 {
-    [SerializeField] private AudioMixer _music;
-    private float value = 1;
+    [SerializeField] private AudioMixer _music; //Music Audiomixer that needs to fade out.
+    private float value = 1; //How loud the music is.
 
     private void OnEnable()
     {
-        this.value = PlayerPrefs.GetFloat("MusicMixerValue");
+        this.value = PlayerPrefs.GetFloat("MusicMixerValue"); //Gets the current value of the AudioMixer.
     }
     private void FixedUpdate()
     {
 
         if (!(this.value < 0.005f)) { this.value -= 0.01f; }
-        if ( this.value < 0.005f ) { this.value = 0.005f; }
+        if ( this.value < 0.005f ) { this.value = 0.005f; } //Reduces the value of the Auiomixer before applying it to the actual mixer, allowing for a fade-outy sound.
         this._music.SetFloat("Volume", Mathf.Log10(this.value) * 20);
 
     }
